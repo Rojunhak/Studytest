697dba45e4d5351a42645bd7ecab82234e9cb5d1
soonook <98567899+soonook@users.noreply.github.com>
2022-02-22 10:05:35 +0900

    Test

---
+++ Study/Study/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static Birth birth;
+        static void Main(string[] args)
+        {
+            birth = new Birth();
+            birth.Name = "나";
+            birth.BrithDate = new DateTime(1994, 02, 15);
+
+
+            Console.WriteLine($"{birth.Name} 나이 : {birth.Age}");
+        }
+    }
+
+    class Birth
+    {
+        public string Name { get; set; }
+        public DateTime BrithDate { get; set; }
+        public int Age
+        {
+            get => (DateTime.Now - BrithDate).Days / 365+1;
+        }
+
+        
+    }
+}
---
+++ Study/StudyTest/Assets/Quternion_LookRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quternion_LookRotation : MonoBehaviour
+{
+    public Transform target;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 relativePos = target.position - transform.position;
+
+        // the second argument, upwards, defaults to Vector3.up
+        Quaternion rotation = Quaternion.Inverse(Quaternion.LookRotation(relativePos, Vector3.up));
+        transform.rotation = rotation;
+        //타겟을 계속 바라보게 하는 함수?
+    }
+}
